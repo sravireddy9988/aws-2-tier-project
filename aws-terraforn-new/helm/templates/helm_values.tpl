cf19e79ec3160054424dd29138daee1978f6ccf0
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2025-07-07 03:03:58 +0530

    Delete aws-terraforn-new directory

--- aws-terraforn-new/helm/templates/helm_values.tpl
+++
@@ -1,9 +0,0 @@
-service:
-  type: NodePort
-
-replicaCount: 2
-
-resources:
-  limits:
-    cpu: 100m
-    memory: 128Mi
--- aws-terraforn-new/rds/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "rds_endpoint" {
-  description = "RDS database endpoint"
-  value       = aws_db_instance.rds_instance.endpoint
-}
-
-output "rds_instance_id" {
-  description = "RDS instance identifier"
-  value       = aws_db_instance.rds_instance.id
-}
